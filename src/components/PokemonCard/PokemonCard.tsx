88af88a202b68ed548b3419e19f02850395be1fb
Brice Macias <bricemacias@brices-macbook-pro.home>
2022-08-10 22:44:27 +0200

    fixing image alignment

--- src/components/PokemonCard/PokemonCard.tsx
+++ src/components/PokemonCard/PokemonCard.tsx
@@ -10,7 +10,7 @@ const PokemonCard = ({ pokemon }: PokemonCardProps) => {
   return (
     <div className="flex justify-center items-center h-80 mx-2 my-10 overflow-visible">
       <div className="relative flex-col transition-all ease-in-out duration-300 bg-slate-50 rounded-2xl flex justify-center items-center h-72 w-96 hover:mb-3  cursor-pointer shadow-sm hover:shadow-lg">
-        <img className="absolute bottom-56 left-1/2" src={pokemon.img} />
+        <img className="absolute bottom-56 left-auto" src={pokemon.img} />
         <div className="text-slate-400 font-bold my-6">{`Nº${pokemon.num}`}</div>
         <div className="font-extrabold mb-10">{pokemon.name}</div>
         <PokemonCardTypeList types={pokemon.type} />
